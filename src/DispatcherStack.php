373fdcd1836b9d43e794a3ce3a1989da6814a592
breman <to.breman@gmail.com>
2015-04-10 04:01:50 +0300

    returns

--- src/DispatcherStack.php
+++ src/DispatcherStack.php
@@ -8,13 +8,13 @@ class DispatcherStack extends Dispatcher {
 
 	public function raise(Event $event) {
 		$this->push($event);
+		return $this;
 	}
 
 	private function processStack() {
 		while ( $event = $this->pop() ) {
 			parent::raise($event);
 		}
-		return $this;
 	}
 
 	private function pop() {
